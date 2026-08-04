ff925eee2a06e1a42025ab43b3efff39d790c4e2
Wesley Cauan <wesleycauan068@gmail.com>
2026-08-03 23:04:17 -0300

    Carregando o css

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,12 @@ module.exports = {
       favicon: path.resolve("src", "assets", "scissors.svg"),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
