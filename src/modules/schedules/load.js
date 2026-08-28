90f8dce5009a809fdffdec27890dee32e0f15478
Wesley Cauan <wesleycauan068@gmail.com>
2026-08-27 22:20:24 -0300

    Buscando os agendamentos na API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
+  // obtem a data do input
   const date = selectedDate.value;
 
+  //busca na api os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
+
+  // renderiza as horas disponiveis
   hoursLoad({ date });
 }
---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,22 @@
+import dayjs from "dayjs";
+import { apiConfig } from "./api-config.js";
+
+export async function scheduleFetchByDay({ date }) {
+  try {
+    // faz a requisição
+    const response = await fetch(`${apiConfig.baseURL}/schedules`);
+
+    // coonverte para json
+    const data = await response.json();
+
+    // filtra os agendamentos pelo dia selecionado
+    const dailySchedules = data.filter((schedule) => {
+      dayjs(date).isSame(schedule.when, "day");
+    });
+
+    return dailySchedules;
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possivel buscar os agendamentos do dia selecionado.");
+  }
+}
